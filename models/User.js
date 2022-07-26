39c76290243874fbecb4e6b050aa8dc00626a5ba
lorreina <slorreina369@gmail.com>
2022-07-26 16:46:33 -0700

    created function to protect passwords

--- models/User.js
+++ models/User.js
@@ -1,8 +1,13 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model{}
+class User extends Model{
+   checkPassword(loginPw){
+      return bcrypt.compareSync(loginPw, this.password);
+   }
+}
 
 // define table columns and configuration
 User.init(
@@ -34,11 +39,22 @@ User.init(
      }
     },
     {
-    sequelize,
-    timestamps:false,
-    freezeTableName:true,
-    underscored:true,
-    modelName:'user'
+      hooks:{
+         //set up beforeCreate lifecycle "hook" functionality
+         async beforeCreate(newUserData){
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+         },
+         async beforeUpdate(updateUserData){
+            updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+            return updateUserData;
+         }
+      },
+      sequelize,
+      timestamps:false,
+      freezeTableName:true,
+      underscored:true,
+      modelName:'user'
     }
 );
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,8 +47,36 @@ router.post('/', (req,res) =>{
     });
 });
 
+router.post('/login', (req,res) =>{
+    // Query operation
+    User.findOne({
+        where:{
+            email:req.body.email
+        }
+    })
+    //add comment syntax in front of this line in the .then()
+    //res.json({user:dbUserData})
+    .then(dbUserData =>{
+        if(!dbUserData){
+            res.status(400).json({message: 'No user with that email address!'});
+            return;
+        }
+        res.json({user:dbUserData})
+
+        //verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json({message:'Incorrect password'});
+            return;
+        }
+        res.json({user:dbUserData, message:'You are now logged in!'});
+
+    });
+});
+
 router.put('/:id', (req,res) =>{
     User.update(req.body, {
+        individualHooks:true,
         where:{
             id:req.params.id
         }
