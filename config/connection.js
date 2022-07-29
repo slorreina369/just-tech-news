2915a4c11784f0b5060d55a5286b18200d0ecdc5
lorreina <slorreina369@gmail.com>
2022-07-29 09:56:53 -0700

    created api routes for post

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,12 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+console.log("presequel")
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host:'localhost',
     dialect:'mysql',
     port:3306    
 });
+console.log("postsequel")
 
 module.exports = sequelize;
